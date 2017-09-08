c74bde9afb2e7665bbaea2ecced2d257148880e0
Bychawski, Michal <michal.bychawski@sap.com>
2017-09-08 03:05:59 +0200

    Define repositories and entities scan, add datasource propertysource

--- spring-boot-app/src/main/java/pl/polsl/aei/inf/mgr/mb/app/CommunicationLayersApplication.java
+++ spring-boot-app/src/main/java/pl/polsl/aei/inf/mgr/mb/app/CommunicationLayersApplication.java
@@ -2,10 +2,16 @@ package pl.polsl.aei.inf.mgr.mb.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "pl.polsl.aei.inf.mgr.mb.*")
+@EnableJpaRepositories(basePackages = "pl.polsl.aei.inf.mgr.mb.repositories")
+@EntityScan(basePackages = "pl.polsl.aei.inf.mgr.mb.model")
+@PropertySource("classpath:datasource.properties")
 public class CommunicationLayersApplication
 {
 
